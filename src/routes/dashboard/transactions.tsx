e1876e8e8a17dbe5e1a4aca447f0a486857d3c91
La Pyae Hmue Aung <lynxquas@gmail.com>
2026-02-19 10:54:12 +0630

    feat: minimal transactiontable, layout and responsive

--- src/routes/dashboard/transactions.tsx
+++ src/routes/dashboard/transactions.tsx
@@ -1,12 +1,45 @@
+import SelectField from '@/components/SelectField';
+import TransactionTable from '@/components/transaction/TransactionTable';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/dashboard/transactions')({
     component: TransactionsPage,
 })
 
-function TransactionsPage() { 
+function TransactionsPage() {
+
     return (
-        <div>TransactionsPage</div>
+        <div className="bg-beige-100 min-h-screen md:px-10 md:pb-4 px-4 pt-6">
+            <h2 className="text-preset-1 font-bold mb-10.5">Transactions</h2>
+
+            <div className="bg-white rounded-xl p-2 lg:p-8">
+                <div className="flex flex-1 justify-between items-center">
+                    <div className="">
+                        <input
+                            placeholder="Search transaction"
+                            name="transaction"
+                            className="px-5 py-3 border border-beige-500 rounded-md"
+                        />
+                    </div>
+
+                    <div className="flex items-center gap-6">
+                        <SelectField name="sort-by" label="Sort by" options={[
+                            { name: "Latest", value: "latest" },
+                            { name: "Oldest", value: "oldest" },
+                            { name: "A to Z", value: "atoz" },
+                            { name: "Z to A", value: "ztoa" },
+                            { name: "Highest", value: "highest" },
+                            { name: "Lowest", value: "lowest" },
+                        ]}
+                        />
+                    </div>
+                </div>
+                <div className="overflow-x-auto">
+                    <TransactionTable/>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
